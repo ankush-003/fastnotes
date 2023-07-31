caf0540af82dd4db44a933024473a0f6e6ac1ad1
ankush-003 <94037471+ankush-003@users.noreply.github.com>
2023-07-31 17:55:24 +0530

    removed env

--- config/db.py
+++ config/db.py
@@ -1,9 +1,6 @@
-import os
 from pymongo import MongoClient
-from dotenv import load_dotenv
-load_dotenv()
 
-MONGO_URI = os.getenv("MONGODB_URI")
+MONGO_URI = "mongodb+srv://Ankush:ganya@learning.id5ibpg.mongodb.net/?retryWrites=true&w=majority"
 conn = MongoClient(MONGO_URI)
 db = conn.mongo_notes
 if __name__ == "__main__":
